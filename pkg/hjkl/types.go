4611f5f50a4ca8931483197e99e4743d17474d0e
Jeffrey Lund <jefflund@gmail.com>
2024-06-06 23:54:05 -0400

    Flesh out types with Vector and int math

--- pkg/hjkl/types.go
+++ pkg/hjkl/types.go
@@ -1,5 +1,7 @@
 package hjkl
 
+import "math"
+
 // Key represents a single keypress.
 type Key rune
 
@@ -18,6 +20,36 @@ type Vector struct {
 // Vec is shorthand for Vector{X: x, Y: y}.
 func Vec(x, y int) Vector {
 	return Vector{X: x, Y: y}
+}
+
+// Add returns the sum of adding another Vector to this one.
+func (a Vector) Add(b Vector) Vector {
+	return Vector{a.X + b.X, a.Y + b.Y}
+}
+
+// Sub returns the sum of subtracting another Vector from this one.
+func (a Vector) Sub(b Vector) Vector {
+	return Vector{a.X - b.X, a.Y - b.Y}
+}
+
+// Neg returns the result of negating the Vector.
+func (a Vector) Neg() Vector {
+	return Vector{-a.X, -a.Y}
+}
+
+// Manhattan returns the L_1 norm of the Vector.
+func (a Vector) Manhattan() int {
+	return Abs(a.X) + Abs(a.Y)
+}
+
+// Euclidean returns the L_2 norm of the Vector.
+func (a Vector) Euclidean() float64 {
+	return math.Hypot(float64(a.X), float64(a.Y))
+}
+
+// Chebyshev returns the L_inf norm of the Vector.
+func (a Vector) Chebyshev() int {
+	return Max(Abs(a.X), Abs(a.Y))
 }
 
 // Color describes the color of a Glyph as a uint8 ANSI 256 color code.
@@ -53,4 +85,53 @@ type Glyph struct {
 // Ch is shorthand for Glyph{Ch: ch, Fg: ColorWhite, Bg: ColorBlack}.
 func Ch(ch rune) Glyph {
 	return Glyph{Ch: ch, Fg: ColorWhite, Bg: ColorBlack}
+}
+
+// Min returns the minimum of two ints.
+func Min(x, y int) int {
+	if y < x {
+		return y
+	}
+	return x
+}
+
+// Max returns the maximum of two ints.
+func Max(x, y int) int {
+	if y > x {
+		return y
+	}
+	return x
+}
+
+// Clamp limits x to the range [a, b].
+func Clamp(a, x, b int) int {
+	if x < a {
+		return a
+	}
+	if x > b {
+		return b
+	}
+	return x
+}
+
+// Abs returns the absolute value of x.
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// Sign returns 1 if x > 0, -1 if x < 0, and 0 if x = 0.
+func Sign(x int) int {
+	return Clamp(-1, x, 1)
+}
+
+// Mod returns x modulo y (not the same as x % y, which is remainder).
+func Mod(x, y int) int {
+	z := x % y
+	if z < 0 {
+		z += y
+	}
+	return z
 }
---
+++ pkg/hjkl/types_test.go
@@ -0,0 +1,278 @@
+package hjkl
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func TestVector_Add(t *testing.T) {
+	cases := []struct {
+		a, b, expected Vector
+	}{
+		{Vector{4, 2}, Vector{2, -1}, Vector{6, 1}},
+		{Vector{4, -2}, Vector{2, -1}, Vector{6, -3}},
+		{Vector{0, 2}, Vector{2, -1}, Vector{2, 1}},
+		{Vector{-3, 2}, Vector{2, -1}, Vector{-1, 1}},
+		{Vector{-3, 2}, Vector{0, 0}, Vector{-3, 2}},
+		{Vector{0, 0}, Vector{4, 9}, Vector{4, 9}},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("%v.Add(%v)", c.a, c.b)
+		t.Run(name, func(t *testing.T) {
+			if actual := c.a.Add(c.b); actual != c.expected {
+				t.Errorf("%s = %v != %v", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestVector_Sub(t *testing.T) {
+	cases := []struct {
+		a, b, expected Vector
+	}{
+		{Vector{4, 2}, Vector{2, -1}, Vector{2, 3}},
+		{Vector{4, -2}, Vector{2, -1}, Vector{2, -1}},
+		{Vector{0, 2}, Vector{2, -1}, Vector{-2, 3}},
+		{Vector{-3, 2}, Vector{2, -1}, Vector{-5, 3}},
+		{Vector{-3, 2}, Vector{0, 0}, Vector{-3, 2}},
+		{Vector{0, 0}, Vector{4, 9}, Vector{-4, -9}},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("%v.Sub(%v)", c.a, c.b)
+		t.Run(name, func(t *testing.T) {
+			if actual := c.a.Sub(c.b); actual != c.expected {
+				t.Errorf("%s = %v != %v", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestVector_Neg(t *testing.T) {
+	cases := []struct {
+		vec, expected Vector
+	}{
+		{Vector{4, 2}, Vector{-4, -2}},
+		{Vector{4, -2}, Vector{-4, 2}},
+		{Vector{0, 2}, Vector{0, -2}},
+		{Vector{-3, 2}, Vector{3, -2}},
+		{Vector{0, 0}, Vector{0, 0}},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("%v.Neg()", c.vec)
+		t.Run(name, func(t *testing.T) {
+			if actual := c.vec.Neg(); actual != c.expected {
+				t.Errorf("%s = %v != %v", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestVector_Manhattan(t *testing.T) {
+	cases := []struct {
+		vec      Vector
+		expected int
+	}{
+		{Vector{4, 6}, 10},
+		{Vector{-4, 6}, 10},
+		{Vector{4, -6}, 10},
+		{Vector{-4, -6}, 10},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("%v.Manhattan()", c.vec)
+		t.Run(name, func(t *testing.T) {
+			if actual := c.vec.Manhattan(); actual != c.expected {
+				t.Errorf("%s == %v != %v", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestVector_Euclidean(t *testing.T) {
+	cases := []struct {
+		vec      Vector
+		expected float64
+	}{
+		{Vector{3, 4}, 5},
+		{Vector{-3, 4}, 5},
+		{Vector{3, -4}, 5},
+		{Vector{-3, -4}, 5},
+		{Vector{2, 4}, math.Sqrt(20)},
+		{Vector{-2, 4}, math.Sqrt(20)},
+		{Vector{2, -4}, math.Sqrt(20)},
+		{Vector{-2, -4}, math.Sqrt(20)},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("%v.Euclidean()", c.vec)
+		t.Run(name, func(t *testing.T) {
+			if actual := c.vec.Euclidean(); actual != c.expected {
+				t.Errorf("%s == %v != %v", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestVector_Chebyshev(t *testing.T) {
+	cases := []struct {
+		vec      Vector
+		expected int
+	}{
+		{Vector{4, 6}, 6},
+		{Vector{-4, 6}, 6},
+		{Vector{4, -6}, 6},
+		{Vector{-4, -6}, 6},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("%v.Chebyshev()", c.vec)
+		t.Run(name, func(t *testing.T) {
+			if actual := c.vec.Chebyshev(); actual != c.expected {
+				t.Errorf("%s == %v != %v", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestMin(t *testing.T) {
+	cases := []struct {
+		x, y, expected int
+	}{
+		{4, 7, 4},
+		{-5, 3, -5},
+		{-4, -3, -4},
+		{4, -2, -2},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("Min(%d, %d)", c.x, c.y)
+		t.Run(name, func(t *testing.T) {
+			if actual := Min(c.x, c.y); actual != c.expected {
+				t.Errorf("%s == %d != %d", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestMax(t *testing.T) {
+	cases := []struct {
+		x, y, expected int
+	}{
+		{4, 7, 7},
+		{-5, 3, 3},
+		{-4, -3, -3},
+		{4, -2, 4},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("Max(%d, %d)", c.x, c.y)
+		t.Run(name, func(t *testing.T) {
+			if actual := Max(c.x, c.y); actual != c.expected {
+				t.Errorf("%s == %d != %d", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestClamp(t *testing.T) {
+	cases := []struct {
+		a, x, b  int
+		expected int
+	}{
+		{-1, -2, 1, -1},
+		{-1, -1, 1, -1},
+		{-1, 0, 1, 0},
+		{-1, 1, 1, 1},
+		{-1, 2, 1, 1},
+		{0, -1, 10, 0},
+		{0, 0, 10, 0},
+		{0, 1, 10, 1},
+		{0, 5, 10, 5},
+		{0, 9, 10, 9},
+		{0, 10, 10, 10},
+		{0, 11, 10, 10},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("Clamp(%d, %d, %d)", c.a, c.x, c.b)
+		t.Run(name, func(t *testing.T) {
+			if actual := Clamp(c.a, c.x, c.b); actual != c.expected {
+				t.Errorf("%s == %d != %d", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestAbs(t *testing.T) {
+	cases := []struct {
+		x, expected int
+	}{
+		{1, 1},
+		{2, 2},
+		{0, 0},
+		{-1, 1},
+		{-2, 2},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("Abs(%d)", c.x)
+		t.Run(name, func(t *testing.T) {
+			if actual := Abs(c.x); actual != c.expected {
+				t.Errorf("%s == %d != %d", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestSign(t *testing.T) {
+	cases := []struct {
+		x, expected int
+	}{
+		{1, 1},
+		{2, 1},
+		{0, 0},
+		{-1, -1},
+		{-2, -1},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("Sign(%d", c.x)
+		t.Run(name, func(t *testing.T) {
+			if actual := Sign(c.x); actual != c.expected {
+				t.Errorf("%s == %d != %d", name, actual, c.expected)
+			}
+		})
+	}
+}
+
+func TestMod(t *testing.T) {
+	cases := []struct {
+		x, y     int
+		expected int
+	}{
+		{-11, 5, 4},
+		{-10, 5, 0},
+		{-9, 5, 1},
+		{-8, 5, 2},
+		{-7, 5, 3},
+		{-6, 5, 4},
+		{-5, 5, 0},
+		{-4, 5, 1},
+		{-3, 5, 2},
+		{-2, 5, 3},
+		{-1, 5, 4},
+		{0, 5, 0},
+		{1, 5, 1},
+		{2, 5, 2},
+		{3, 5, 3},
+		{4, 5, 4},
+		{5, 5, 0},
+		{6, 5, 1},
+		{7, 5, 2},
+		{8, 5, 3},
+		{9, 5, 4},
+		{10, 5, 0},
+		{11, 5, 1},
+	}
+	for _, c := range cases {
+		name := fmt.Sprintf("Mod(%d, %d)", c.x, c.y)
+		t.Run(name, func(t *testing.T) {
+			if actual := Mod(c.x, c.y); c.expected != actual {
+				t.Errorf("%s == %d != %d", name, actual, c.expected)
+			}
+		})
+	}
+}
