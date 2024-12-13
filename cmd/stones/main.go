c1b2881d8b863a6fea29951b497d0e899a475fb3
Jeffrey Lund <jefflund@gmail.com>
2024-12-13 16:52:39 -0500

    Make a walking-at demo

--- cmd/stones/main.go
+++ cmd/stones/main.go
@@ -2,23 +2,67 @@ package main
 
 import "github.com/jefflund/stones/pkg/hjkl"
 
-type Game struct{}
+const (
+	COLS = 80
+	ROWS = 24
+)
 
-func (Game) Update(ks []hjkl.Key) error {
-	if len(ks) > 0 {
-		return hjkl.Termination
+var (
+	WALL  = hjkl.Ch('#')
+	FLOOR = hjkl.Ch('.')
+)
+
+type Game struct {
+	Hero  hjkl.Vector
+	Level map[hjkl.Vector]hjkl.Glyph
+}
+
+func NewGame() *Game {
+	hero := hjkl.Vec(40, 12)
+	level := make(map[hjkl.Vector]hjkl.Glyph)
+	for x := 0; x < COLS; x++ {
+		for y := 0; y < ROWS; y++ {
+			tile := FLOOR
+			if x == 0 || x == COLS-1 || y == 0 || y == ROWS-1 {
+				tile = WALL
+			}
+			level[hjkl.Vec(x, y)] = tile
+		}
+	}
+	return &Game{hero, level}
+}
+
+func (g *Game) Update(ks []hjkl.Key) error {
+	var delta hjkl.Vector
+	for _, k := range ks {
+		switch k {
+		case hjkl.KeyEsc, hjkl.KeyCtrlC:
+			return hjkl.Termination
+		case 'h':
+			delta = hjkl.Vec(-1, 0)
+		case 'j':
+			delta = hjkl.Vec(0, 1)
+		case 'k':
+			delta = hjkl.Vec(0, -1)
+		case 'l':
+			delta = hjkl.Vec(1, 0)
+		}
+	}
+	if dst := g.Hero.Add(delta); g.Level[dst] == FLOOR {
+		g.Hero = dst
 	}
 	return nil
 }
 
-func (Game) Draw(c hjkl.Canvas) {
-	for i, ch := range "Hello, World!" {
-		c.Blit(hjkl.Vec(i, 0), hjkl.Ch(ch))
+func (g *Game) Draw(c hjkl.Canvas) {
+	for v, g := range g.Level {
+		c.Blit(v, g)
 	}
+	c.Blit(g.Hero, hjkl.Ch('@'))
 }
 
 func main() {
-	if err := hjkl.Run(Game{}); err != nil {
+	if err := hjkl.Run(NewGame()); err != nil {
 		panic(err)
 	}
 }
